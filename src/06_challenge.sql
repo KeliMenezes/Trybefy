b5ae2555cea5b79778dba840fbc537a290a21ec9
keli <kr.menezes@yahoo.com>
2023-11-24 09:41:41 -0300

    Requisito 06

---
+++ src/06_challenge.sql
@@ -0,0 +1,3 @@
+UPDATE Trybefy.users SET active = true WHERE full_name = "Andressa";
+UPDATE Trybefy.users SET active = false WHERE full_name = "Rogério";
+UPDATE Trybefy.users SET plan_id = 2 WHERE full_name = "Camila";
