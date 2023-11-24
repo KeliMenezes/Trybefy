ee782fb720a3494c33eab5d7f8e5c446ea6a4c29
keli <kr.menezes@yahoo.com>
2023-11-24 12:04:37 -0300

    Requisito 26

---
+++ src/26_challenge.sql
@@ -0,0 +1,12 @@
+SELECT name AS "Artista",
+COUNT(history_play_songs.id) AS "Quantidade de músicas reproduzidas"
+FROM artists
+JOIN albums
+ON artists.id = albums.artist_id
+JOIN songs
+ON albums.id = songs.album_id
+JOIN history_play_songs
+ON songs.id = history_play_songs.song_id
+GROUP BY name
+HAVING COUNT(history_play_songs.id) > 10
+ORDER BY name;
