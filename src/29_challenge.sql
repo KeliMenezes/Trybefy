51f2b0f7134ad74ea44516382e3128ecd0a9e10c
keli <kr.menezes@yahoo.com>
2023-11-24 12:30:59 -0300

    Requisito 29

---
+++ src/29_challenge.sql
@@ -0,0 +1,8 @@
+SELECT artists.name AS "Artista"
+FROM artists
+JOIN artists_followers
+ON artists.id = artists_followers.artist_id
+GROUP BY artists.name
+ORDER BY COUNT(artists_followers.artist_id) DESC
+LIMIT 1;
+
